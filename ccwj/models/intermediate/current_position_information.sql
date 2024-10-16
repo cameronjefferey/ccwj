ca78a4c3f1f89e2b2df79e09636768c78d147847
cameronjefferey <cameronjsmith23@gmail.com>
2024-10-16 15:32:51 -0700

    testing fr

---
+++ ccwj/models/intermediate/current_position_information.sql
@@ -0,0 +1,14 @@
+select 
+   symbol,
+   trade_symbol,
+   option_security_type,
+   quantity,
+   price,
+   market_value,
+   cost_basis,
+   gain_or_loss_dollar,
+   security_type,
+   margin_requirement,
+   case 
+      when quantity
+from {{ ref('current')}}
